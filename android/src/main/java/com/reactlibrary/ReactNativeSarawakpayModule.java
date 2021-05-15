d61fdbebd0ae61c1dd3af47aa8923e08cff0fa76
Nicholas Jong <nicholas@ooomax.com>
2021-05-15 23:16:50 +0800

    testing

--- android/src/main/java/com/reactlibrary/ReactNativeSarawakpayModule.java
+++ android/src/main/java/com/reactlibrary/ReactNativeSarawakpayModule.java
@@ -5,6 +5,12 @@ package com.reactlibrary;
 import android.content.pm.PackageManager;
 import android.content.Intent;
 
+// below to be delete is no use 
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -17,18 +23,47 @@ import sharepay.paylibrary.SarawakAPI;
 import sharepay.paylibrary.SarawakPay;
 import sharepay.paylibrary.SarawakPayCallback;
 
+import my.gov.sarawak.paybills.IPayAidlInterface;
+import my.gov.sarawak.paybills.IPayAidlInterface.Stub;
+import sharepay.paylibrary.BaseCallbackBean.Builder;
+
 import android.util.Log;
 
-public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule implements SarawakPayCallback  {
-
+public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule   {
     private final ReactApplicationContext reactContext;
     private SarawakAPI mFactory;
+    private IPayAidlInterface mIPayAidlInterface;
+    private final boolean mIsBind;
     Promise promise;
+
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            ReactNativeSarawakpayModule.this.mIPayAidlInterface = Stub.asInterface(service);
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
 
     public ReactNativeSarawakpayModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        mFactory = SarawakPay.createFactory(this.reactContext);
+
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("my.gov.sarawak.paybills", "my.gov.sarawak.paybills.pay.SarawakPayPayService"));
+        this.mIsBind = this.reactContext.bindService(intent, this.mServiceConnection, 1);
+    }
+
+    @ReactMethod
+    public void test(Callback callback) {
+        // TODO: Implement some actually useful functionality
+        if (this.mIPayAidlInterface != null && this.mIsBind) {
+            String targetPackName = this.mIPayAidlInterface.getTargetPackName();
+            callback.invoke("ok ok" + targetPackName);
+            return ;
+        }
+        callback.invoke("Cannot show");
     }
 
     @Override
@@ -36,11 +71,18 @@ public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule impl
         return "ReactNativeSarawakpay";
     }
 
-    @Override
-    public void payResult(BaseCallbackBean baseCallbackBean) {
-        //其中baseCallbackBean封装了相应的请求信息
-        baseCallbackBean.getFlag();
-    }
+
+
+    // @ReactMethod
+    // public void sendRequest(String orderInfo) {
+    //     Intent intent;
+    //     intent = new Intent("my.gov.sarawak.paybills.login.InitActivity");
+    //     intent.putExtra("orderInfo", orderInfo);
+    //     intent.putExtra("payFlag", "sharePay");
+    //     intent.putExtra("pkgName", this.reactContext.getPackageName());
+    //     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    //     this.reactContext.startActivity(intent);
+    // }
 
     @ReactMethod
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
@@ -48,13 +90,6 @@ public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule impl
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
 
-    @ReactMethod
-    public void sendRequest(String data) {
-        mFactory.sendReq(data, this);
-        Log.d("pkgName", "Cannot resolve info for" + data);
-        String packageName = reactContext.getPackageName();
-        Log.d("pkgName", "Cannot resolve info for" + packageName);
-    }
 
     @ReactMethod
 	public void isPackageInstalled(String packageName, final Promise promise) {
--- example/App.js
+++ example/App.js
@@ -10,7 +10,7 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button, Alert } from 'react-native';
-import ReactNativeSarawakpay, {OpenSPay} from 'react-native-sarawakpay';
+import ReactNativeSarawakpayModule, {OpenSPay} from 'react-native-sarawakpay';
 
 const orderEncryptedString = "MDAwMjU2R8/YMSpuAD8bFzmeozXOtjCRcvldFFwOzIP9VSWJ/mCJPgOgr/DA2WQw7O5E6pjCxbPWyXkm3NZFKyqFEsNjzy9m2uACYg7df4E+0tWiFSFpPV3G/PcXwkZ2wnUJBZZK5qSJax9k6kpiahkIcui484Xk7zXHnVm9CB6OvOS11T1tD3Q7gWsezNZPXw93orJtEQvmpmI6FgCT2MN4Ql+EwcjtCCLFSTVfVMzaXQjamq+ctTmYfqLw5yBSG+7j2of+y/KtjdyEYyzjilurF8SFH7RnH0nkt+1t9vmFFRqmd4IcsigDivgBMhO5EOwuwqDxTtFkfti2itPhREbLWpdknczxirMsc7aFFY2YwM/vhfOzpCoUzsAOP80gkIvJhVzIw8B8Kd8hWXfAOsielSsW6jTTyS9jeiL0d2ff05iNUxyZPkF00vJO2S5pUQhjx9IdRx7VlMQl/cxexOdyHVOe8Fh6wgCviXdhHma75g/zdkglBNcKjbAW/7xIXIKUpp9NxhQPPoNTxHBzUi5DmYDkKUVNJqtEtJn/9QO/DsMWh0cuwblKvIRkoi8KT9d6Mo7BM9RIDTRqD9mLCjHElkhySl7rDPIUbIo2/cfH+kKooin4HLfMD7R09PU+dvZr8PvUtOPth1IYLxj+ScYE7Dtd7wVDc8qLPZb3Au83I1F+3FIST9uZcesnbRYTzquhYzM0TyoMrWAP1fPId9vmD4xvD/Kk6Q8q1/LEx9UTLN33LCVD1y6NnFE1b5JC8irL/lf4isYnLdVO3bRYmJqwGqRWgM2RNP/PwdEkX2eJ8JPoxgYr+cjkVvy4nKVshARWmCgpIl28FuLPfHYjuH3du4irU/Hv0lwCuWke9lMK5VRfrq2R0Qgp8E/AT3eb4aTB6OWUpg1LmDKBJHlRUw0IaIcYhtQqOMWgfU+ATe42r3OuW6+tIkVM8ewzmlUl7rFZ+4TJkY06Nyd3qX6ZY3xxJ0KyHPl0iL5Bx1BJS65s43TwjbOc/5jj0WC9XSGgxEIu9ROkUqFVr/B0qo5nuKZogDn3gfLjAzifmm2G1kuLRPggIQE="
   
@@ -19,30 +19,21 @@ export default class App extends Component<{}> {
     status: 'starting',
     message: '--'
   };
-  componentDidMount() {
-    ReactNativeSarawakpay.sampleMethod('Testing', 123, (message) => {
-      this.setState({
-        status: 'native callback received',
-        message
-      });
-    });
-  }
+  // componentDidMount() {
+  //   ReactNativeSarawakpay.sampleMethod('Testing', 123, (message) => {
+  //     this.setState({
+  //       status: 'native callback received',
+  //       message
+  //     });
+  //   });
+  // }
 
-
-  // const onPress = () => {
-  //   // SpayModule.createCalendarEvent('Nichoals', 'And go to location');
-  //   SpayModule.sendRequest(orderEncryptedString)
-  // };
   onPress = () => {
-    // ReactNativeSarawakpay.sendRequest(orderEncryptedString)
-    OpenSPay(orderEncryptedString)
 
-    // ReactNativeSarawakpay.sampleMethod('Testing', 123, (message) => {
-    //   this.setState({
-    //     status: 'native callback received',
-    //     message
-    //   });
-    // });
+    // OpenSPay(orderEncryptedString)
+    ReactNativeSarawakpayModule.test(res => {
+      console.log(res)
+    })
   }
 
   render() {
