f013d872ee4978f17caf6696f0f35b3080961afd
Nicholas Jong <nicholas@ooomax.com>
2021-07-21 16:45:22 +0800

    Still having bug when call SPay

--- android/src/main/java/com/reactlibrary/ReactNativeSarawakpayModule.java
+++ android/src/main/java/com/reactlibrary/ReactNativeSarawakpayModule.java
@@ -2,15 +2,64 @@ package com.reactlibrary;
 
 import android.util.Log;
 import android.content.Intent;
+import android.app.Activity;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReactContext;
 
 import sharepay.paylibrary.BaseCallbackBean;
 import sharepay.paylibrary.SarawakAPI;
 import sharepay.paylibrary.SarawakPay;
 import sharepay.paylibrary.SarawakPayCallback;
+
+public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule implements SarawakPayCallback  {
+    private ReactContext reactContext;
+    private SarawakAPI mFactory;
+    Promise promise;
+
+    public ReactNativeSarawakpayModule(ReactApplicationContext context) {
+        super(context);
+        reactContext = context;
+    }
+
+    @Override
+    public String getName() {
+        return "ReactNativeSarawakpay";
+    }
+
+    @Override
+    public void payResult(BaseCallbackBean baseCallbackBean) {
+        //其中baseCallbackBean封装了相应的请求信息
+        Log.d("payResult", baseCallbackBean.getFlag());
+        baseCallbackBean.getFlag();
+    }
+
+    @ReactMethod
+    public void sendRequest(String data) {
+        final Activity currentActivity = getCurrentActivity();
+
+        mFactory = SarawakPay.createFactory(currentActivity);
+        mFactory.sendReq(data, this);
+        Log.d("spay", "Cannot resolve info for" + data);
+        String packageName = reactContext.getPackageName();
+        Log.d("pkgName", "Cannot resolve info for" + packageName);
+        return;
+    }
+
+
+    @ReactMethod
+    public void isPackageInstalled(String packageName, final Promise promise) {
+        Intent sendIntent = reactContext.getPackageManager().getLaunchIntentForPackage(packageName);
+        if (sendIntent == null) {
+            promise.resolve(false);
+            return;
+        }
+        promise.resolve(true);
+    }
+}
 
 
 // public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule implements SarawakPayCallback  {
@@ -61,49 +110,3 @@ import sharepay.paylibrary.SarawakPayCallback;
 
 // }
 
-public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule implements SarawakPayCallback  {
-
-    private final ReactApplicationContext reactContext;
-    private SarawakAPI mFactory;
-
-    Promise promise;
-
-    public ReactNativeSarawakpayModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
-
-		
-    }
-
-    @Override
-    public String getName() {
-        return "ReactNativeSarawakpay";
-    }
-
-    @Override
-    public void payResult(BaseCallbackBean baseCallbackBean) {
-        //其中baseCallbackBean封装了相应的请求信息
-        baseCallbackBean.getFlag();
-    }
-
-    @ReactMethod
-    public void sendRequest(String data) {
-        mFactory = SarawakPay.createFactory(getCurrentActivity());
-        mFactory.sendReq(data, this);
-        Log.d("pkgName", "Cannot resolve info for" + data);
-        String packageName = reactContext.getPackageName();
-        Log.d("pkgName", "Cannot resolve info for" + packageName);
-    }
-
-    @ReactMethod
-    public void isPackageInstalled(String packageName, final Promise promise) {
-        Intent sendIntent = reactContext.getPackageManager().getLaunchIntentForPackage(packageName);
-        if (sendIntent == null) {
-            promise.resolve(false);
-            return;
-        }
-        promise.resolve(true);
-    }
-
-
-}
--- android/src/main/java/com/reactlibrary/ReactNativeSarawakpayPackage.java
+++ android/src/main/java/com/reactlibrary/ReactNativeSarawakpayPackage.java
@@ -6,18 +6,23 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import java.util.Arrays;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 
 
 public class ReactNativeSarawakpayPackage implements ReactPackage {
-
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new ReactNativeSarawakpayModule(reactContext));
-    }
+       List<NativeModule> modules = new ArrayList<>();
+
+       modules.add(new ReactNativeSarawakpayModule(reactContext));
+
+       return modules;
+   }
+
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
