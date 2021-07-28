182405474c011e6d3e8c15f158a58ce3faaf8d45
Nicholas Jong <nicholas@ooomax.com>
2021-07-28 16:30:09 +0800

    spay fixed

--- android/src/main/java/com/reactlibrary/ReactNativeSarawakpayModule.java
+++ android/src/main/java/com/reactlibrary/ReactNativeSarawakpayModule.java
@@ -1,26 +1,33 @@
-// ReactNativeSarawakpayModule.java
-
 package com.reactlibrary;
 
-import android.content.pm.PackageManager;
+import android.util.Log;
 import android.content.Intent;
+import android.app.Activity;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReactContext;
+
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.NativeModule;
 
-import android.util.Log;
+import sharepay.paylibrary.BaseCallbackBean;
+import sharepay.paylibrary.SarawakAPI;
+import sharepay.paylibrary.SarawakPay;
+import sharepay.paylibrary.SarawakPayCallback;
 
-public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule {
-
-    private final ReactApplicationContext reactContext;
+public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule implements SarawakPayCallback, LifecycleEventListener  {
+    private ReactContext reactContext;
+    private SarawakAPI mFactory;
     Promise promise;
 
-    public ReactNativeSarawakpayModule(ReactApplicationContext reactContext) {
-        super(reactContext);
-        this.reactContext = reactContext;
+    public ReactNativeSarawakpayModule(ReactApplicationContext context) {
+        super(context);
+        reactContext = context;
+        reactContext.addLifecycleEventListener(this);
     }
 
     @Override
@@ -28,26 +35,51 @@ public class ReactNativeSarawakpayModule extends ReactContextBaseJavaModule {
         return "ReactNativeSarawakpay";
     }
 
-    @ReactMethod
-    public void sendRequest(String orderInfo) {
-        Intent intent;
-        intent = new Intent("my.gov.sarawak.paybills.pay.AuthActivity");
-        intent.putExtra("orderInfo", orderInfo);
-        intent.putExtra("payFlag", "sharePay");
-        intent.putExtra("pkgName", this.reactContext.getPackageName());
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        this.reactContext.startActivity(intent);
+    @Override
+    public void payResult(BaseCallbackBean baseCallbackBean) {
+        //其中baseCallbackBean封装了相应的请求信息
+        Log.d("payResult", baseCallbackBean.getFlag());
+        baseCallbackBean.getFlag();
     }
 
     @ReactMethod
-	public void isPackageInstalled(String packageName, final Promise promise) {
-		Intent sendIntent = this.reactContext.getPackageManager().getLaunchIntentForPackage(packageName);
-		if (sendIntent == null) {
-			promise.resolve(false);
-			return;
-		}
-		promise.resolve(true);
-	}
-    
+    public void sendRequest(String data) {
+        // final Activity currentActivity = getCurrentActivity();
+        // mFactory = SarawakPay.createFactory(currentActivity);
 
+        mFactory.sendReq(data, this);
+        Log.d("spay", "Cannot resolve info for" + data);
+        String packageName = reactContext.getPackageName();
+        Log.d("pkgName", "Cannot resolve info for" + packageName);
+    }
+
+
+    @ReactMethod
+    public void isPackageInstalled(String packageName, final Promise promise) {
+        Intent sendIntent = reactContext.getPackageManager().getLaunchIntentForPackage(packageName);
+        if (sendIntent == null) {
+            promise.resolve(false);
+            return;
+        }
+        promise.resolve(true);
+    }
+
+    @Override
+    public void onHostResume() {
+       if (mFactory == null) {
+           mFactory = SarawakPay.createFactory(this.getCurrentActivity());
+       }
+    }
+    @Override
+    public void onHostPause() {
+       // Activity `onPause`
+    }
+    @Override
+    public void onHostDestroy() {
+       if (mFactory != null) {
+           mFactory.onDestroy();
+       }
+    }
 }
+
+
